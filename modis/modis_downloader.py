8bfc026bc6e991df6b9fa9dbe8d6a3002881625e
Manuel Cabeza Gallucci <42854238+ManuelCabeza@users.noreply.github.com>
2022-10-27 10:18:14 +0200

    Modification so it works on windows

--- modis/modis_downloader.py
+++ modis/modis_downloader.py
@@ -3,9 +3,10 @@ from pymodis import downmodis
 import numpy as np
 import pymp
 import time
-from utils import *
+# from utils import *
 from argparse import ArgumentParser
-
+# import torchmultiprocessing as mp   
+    
 
 def MODIS_Parallel_Downloader(year,product, num_threads, user="projet3a", password="Projet3AIMT"):
     sensor        = product.split(".")[0]
@@ -20,26 +21,27 @@ def MODIS_Parallel_Downloader(year,product, num_threads, user="projet3a", passwo
     os.makedirs(tifs_4km_path,exist_ok=1)
 
     # Download data with multi-core
-    with pymp.Parallel(num_threads) as p:
+    # Parallel doesnt work in windows
+    # with pymp.Parallel(num_threads) as p:
     # if 1:
-        for month in p.range(1, 13):
-            startdate = "{}-{}-01".format(str(year),str(month).zfill(2))
-            if month != 12: 
-                enddate = "{}-{}-01".format(str(year),str(month+1).zfill(2))
-            else:
-                enddate = "{}-{}-31".format(str(year),str(month).zfill(2))
+    for month in range(1, 13):
+        startdate = "{}-{}-01".format(str(year),str(month).zfill(2))
+        if month != 12: 
+            enddate = "{}-{}-01".format(str(year),str(month+1).zfill(2))
+        else:
+            enddate = "{}-{}-31".format(str(year),str(month).zfill(2))
 
-            start_time = time.time()
-            print("Start to download {} From {} to {}".format(hdfs_path, startdate,enddate))
-            try:
-                modisDown = downmodis.downModis(user=user,password=password,product=product,destinationFolder=hdfs_path, tiles=tiles, today=startdate, enddate=enddate)
-                modisDown.connect()
-                modisDown.downloadsAllDay()
-                # modisDown.downloadsAllDay(clean=True, allDays=True)
-            except:
-                print("Download Error {} From {} to {}".format(hdfs_path, startdate,enddate))
-            print("Finish download {} From {} to {}, time cost: {:.4f}".format(hdfs_path, startdate,enddate,time.time()-start_time))
-            
+        start_time = time.time()
+        print("Start to download {} From {} to {}".format(hdfs_path, startdate,enddate))
+        try:
+            modisDown = downmodis.downModis(user=user,password=password,product=product,destinationFolder=hdfs_path, tiles=tiles, today=startdate, enddate=enddate)
+            modisDown.connect()
+            modisDown.downloadsAllDay()
+            # modisDown.downloadsAllDay(clean=True, allDays=True)
+        except:
+            print("Download Error {} From {} to {}".format(hdfs_path, startdate,enddate))
+        print("Finish download {} From {} to {}, time cost: {:.4f}".format(hdfs_path, startdate,enddate,time.time()-start_time))
+        
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -50,7 +52,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     years = list(np.arange(args.year_begin, args.year_end))
-    products = ["MOD11A1.061", "MOD13A2.061"] # LST: "MOD11A1.061", NDVI_1km: "MOD13A2.061", NDVI_250m: "MOD13Q1.061"
+    products = ["MOD11A1.061", "MOD13Q1.061"] # LST: "MOD11A1.061", NDVI_1km: "MOD13A2.061", NDVI_250m: "MOD13Q1.061"
     tiles = "h18v04" # tiles to download, France is in h17v04 and h18v04 , string of tiles separated by comma
     num_threads = 6 # Cores number to use 
     for year in years:
