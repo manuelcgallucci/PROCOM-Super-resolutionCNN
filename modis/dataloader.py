ca54e5977f8c9e5541841f04b593ccf16cc2a257
Manuel CABEZA GALLUCCI <m21cabez@sl-tp-br-024.imta.fr>
2022-12-08 10:26:41 +0100

    Improved dataloader

--- modis/dataloader.py
+++ modis/dataloader.py
@@ -2,18 +2,16 @@ from torch.utils.data import Dataset
 import numpy as np
 
 # Define DataLoader
-class DatasetCustom(Dataset):
-    def __init__(self, data):
+class DataLoaderCustom(Dataset):
+    def __init__(self, data_lst, data_nvdi):
         # Data is an array of 2d images of size N x w x h 
         self.data_lst = data_lst
         self.data_nvdi = data_nvdi
-
-        assert self.data_lst.shape[0] == self.data_nvdi.shape[0], "NVDI and LST number of images is different. Indexing will break"
-
+        
+        # assert self.data_lst.shape[0] == self.data_nvdi.shape[0], "NVDI and LST number of images is different. Indexing will break"
         
     def __len__(self):
         return self.data_lst.shape[0]
     
     def __getitem__(self, index):
-
-        return (self.data_lst[index,:,:], self.data_nvdi[index,:,:]) 
+        return self.data_lst[index,:,:], self.data_nvdi[index/2,:,:]
